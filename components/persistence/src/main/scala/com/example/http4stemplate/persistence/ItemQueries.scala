bc63104e7fe35696fcd8f85af502e1399bc25286
Carlos Pinho <carlosdspinho@gmail.com>
2018-11-18 15:51:03 +0000

    Add initial item queries logic

---
+++ components/persistence/src/main/scala/com/example/http4stemplate/persistence/ItemQueries.scala
@@ -0,0 +1,35 @@
+package com.example.http4stemplate.persistence
+
+import com.example.http4stemplate.persistence.utils.{DB, DBMappers}
+import slick.dbio.Effect
+import slick.jdbc.JdbcProfile
+
+
+case class ItemIdentifier(id: Long)
+
+case class Item(id: ItemIdentifier, name: String, value: Double)
+
+
+class ItemQueries[F[_]: Effect](dbComponent: DB) {
+  //TODO: Add item queries
+}
+
+
+class ItemTable(protected val driver: JdbcProfile) extends DBMappers {
+
+  import driver.api._
+
+  class ItemTableDef(tag: Tag) extends Table[Item](tag, "item") {
+    val itemId = column[ItemIdentifier]("problem_id")
+    val name = column[String]("name")
+    val value = column[Double]("value")
+
+    def * =
+      (itemId, name, value) <> (Item.tupled, Item.unapply)
+
+  }
+
+  protected val tableQuery = TableQuery[ItemTableDef]
+
+}
+
