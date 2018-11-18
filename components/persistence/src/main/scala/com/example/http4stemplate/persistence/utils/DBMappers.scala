aa43c2d09d83990911d3016e49b66d816af71ef3
Carlos Pinho <carlosdspinho@gmail.com>
2018-11-18 15:51:23 +0000

    Add mapper to itemIdentifier

--- components/persistence/src/main/scala/com/example/http4stemplate/persistence/utils/DBMappers.scala
+++ components/persistence/src/main/scala/com/example/http4stemplate/persistence/utils/DBMappers.scala
@@ -2,6 +2,7 @@ package com.example.http4stemplate.persistence.utils
 
 import java.sql.Timestamp
 
+import com.example.http4stemplate.persistence.ItemIdentifier
 import org.joda.time.DateTime
 import slick.ast.BaseTypedType
 import slick.jdbc.{JdbcProfile, JdbcType}
@@ -16,4 +17,6 @@ trait DBMappers {
     MappedColumnType
       .base[DateTime, Timestamp](dateTime => new Timestamp(dateTime.getMillis), date => new DateTime(date))
 
+  implicit def itemIdentifierMapper: JdbcType[ItemIdentifier] with BaseTypedType[ItemIdentifier] =
+    MappedColumnType.base[ItemIdentifier, Long](_.id, ItemIdentifier)
 }
